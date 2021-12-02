f1f13b893466e27cb5ef3c3b21023ea6ebd1ae35
Jordan Grace <jordan.t.grace@gmail.com>
2021-12-01 20:13:37 -0500

    Cleaned up and 1.2 setup

--- utils.jl
+++ utils.jl
@@ -1,13 +1,13 @@
-function read_to_array(file)
+function read_to_array(path::String, type::Type)
     result = []
 
-    open(file) do f       
+    open(path) do f       
         # read till end of file
         while ! eof(f) 
        
             # read a new / next line for every iteration          
             s = readline(f)
-            input = parse(Int64, s)
+            input = parse(type, s)
             push!(result, input)
         end
     end

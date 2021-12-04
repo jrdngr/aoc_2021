a0d76700c936a93cb3af1d2042f2c228654b6104
Jordan Grace <jordan.t.grace@gmail.com>
2021-12-03 22:32:46 -0500

    Halfway done with 3
    Have to remove items after each iteration

--- day2.jl
+++ day2.jl
@@ -11,9 +11,11 @@ inputs = read_to_array("inputs/2_1", String)
 # forward X increases the horizontal position by X units.
 # down X increases the depth by X units.
 # up X decreases the depth by X units.
-# Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+# Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of 
+# what you might expect.
 
-# The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+# The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going.
+# For example:
 
 # forward 5
 # down 5
@@ -29,9 +31,11 @@ inputs = read_to_array("inputs/2_1", String)
 # up 3 decreases your depth by 3, resulting in a value of 2.
 # down 8 adds 8 to your depth, resulting in a value of 10.
 # forward 2 adds 2 to your horizontal position, a total of 15.
-# After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+# After following these instructions, you would have a horizontal position of 15 and a depth of 10. 
+# (Multiplying these together produces 150.)
 
-# Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+# Calculate the horizontal position and depth you would have after following the planned course. 
+# What do you get if you multiply your final horizontal position by your final depth?
 
 struct Forward end
 struct Up end
